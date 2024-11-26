f2c9fe608d99fc3ca444925c6ac0883f4faa9710
Huseyn Babakhanov <69239990+HuseynBabakhanov@users.noreply.github.com>
2024-11-26 16:30:07 +0300

    Second Lesson

---
+++ lesson2.py
@@ -0,0 +1,16 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = ""
+age = 25
+gpa = 3.2
+is_student = True
+
+
+name = bool(name)
+
+print(name)
+
+
+
+
