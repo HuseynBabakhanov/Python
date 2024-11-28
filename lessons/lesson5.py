c3cae6aeaca68bcea1282f152c448625320f4ec5
Huseyn Babakhanov <69239990+HuseynBabakhanov@users.noreply.github.com>
2024-11-28 15:14:17 +0300

    Lesson about  "If statements"

---
+++ lessons/lesson5.py
@@ -0,0 +1,42 @@
+# if = Do some code IF some condition is True
+#      Else do something else
+
+#########################Example1########################
+# age = int(input("Enter your age: "))
+
+ 
+# if age>= 100:
+#     print("You are too old to sign up!")
+# elif age >= 18:
+#     print("You are now signed up!")
+# elif age < 0:
+#     print("You haven't been born yet!")
+# else:
+#     print("You must be 18+ to sign up!")
+
+
+#########################Example2########################
+# response = input("Would you like food? (Y/N): ")
+# if response == "Y":
+#     print("Have some food!")
+# else:
+#     print("No food for you!")
+
+#########################Example3########################
+
+# name = input("Enter your name: ")
+
+# if name == "":
+#     print("You did not type in your name!")
+# else:
+#     print(f"Hello {name}")
+
+
+
+#########################Example4########################
+# online = True
+
+# if online:
+#     print("The user is online")
+# else:
+#     print("The user is offline")
